e7c30528c53f063ce55ba89cb1b0a879ed1f3172
z <zshoals@gmail.com>
2021-12-03 14:46:02 -0600

    add memory tool

---
+++ Sources/tectonic/util/memkit.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define unsigned char tec_byte_t
+
+#define TEC_KILOBYTES(amount) ((amount) * 1024)
+#define TEC_MEGABYTES(amount) ((amount) * 1024 * 1024)
+#define TEC_GIGABYTES(amount) ((amount) * 1024 * 1024 * 1024)
