b37770d0d548bb2decdd678a8858ec7c1ea8cab0
z <zshoals@gmail.com>
2022-01-26 19:41:15 -0600

    fixup memkit bytes and style

--- Sources/tectonic/util/memkit.h
+++ Sources/tectonic/util/memkit.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define unsigned char tec_byte_t
+#define tec_byte_t unsigned char 
 
 #define TEC_KILOBYTES(amount) ((amount) * 1024)
-#define TEC_MEGABYTES(amount) ((amount) * 1024 * 1024)
-#define TEC_GIGABYTES(amount) ((amount) * 1024 * 1024 * 1024)
+#define TEC_MEGABYTES(amount) ((TEC_KILOBYTES(amount)) * 1024)
+#define TEC_GIGABYTES(amount) ((TEC_MEGABYTES(amount)) * 1024)
