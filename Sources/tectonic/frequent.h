c7d376605d02ff8f2131c20699c94e4c2531eaeb
z <zshoals@gmail.com>
2022-01-27 20:48:23 -0600

    add useful defines

--- Sources/tectonic/frequent.h
+++ Sources/tectonic/frequent.h
@@ -2,3 +2,9 @@
 
 #define local_data static
 #define local_func static
+
+#define DEREF(symbol) (*(symbol))
+
+//Macro expander
+#define TEC_EXPANDER_IMPL(ex) #ex
+#define TEC_EXPANDER(ex) TEC_EXPANDER_IMPL(ex)
