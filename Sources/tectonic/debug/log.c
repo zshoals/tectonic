b61af76910e13bee2dd7bf777ecd76f252b3f0c6
z <zshoals@gmail.com>
2022-01-30 08:02:05 -0600

    Fix logging FOR ACTUALLY REAL this time

--- Sources/tectonic/debug/log.c
+++ Sources/tectonic/debug/log.c
@@ -3,6 +3,8 @@
 #include <stdarg.h>
 #include "../util/string.h"
 #include "../frequent.h"
+#include <stddef.h>
+#include <assert.h>
 
 //#define TEC_LOG_INFO_PREFIX "[TEC-INFO]::[" TEC_LOG_MODULE_NAME "] "
 //#define TEC_LOG_INFO_VERBOSE_PREFIX "[TEC-INFO]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] "
@@ -13,96 +15,98 @@
 //#define TEC_LOG_ERROR_PREFIX "[TEC-ERROR]::[" TEC_LOG_MODULE_NAME "] "
 //#define TEC_LOG_ERROR_VERBOSE_PREFIX "[TEC-ERROR]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] "
 
-local_func void
-tec_log_modify(char const * format, char const * prefix_string, kinc_log_level_t level, va_list args)
+//Here resides a painful lesson about va_args:
+//the second parameter to va_start is the ADDRESS of the LAST PARAMETER IN THE FUNCTION
+//Proof that reading is indeed the most powerful ability
+
+void
+tec_log_modify(char const * prefix_string, char const * format, kinc_log_level_t level, va_list args)
 {
-	tec_string_t new_format = tec_string_create(prefix_string);
-	tec_string_t original = tec_string_create(format);
-
-	char buffer[8192];
-	tec_string_t final = tec_string_append(&buffer, new_format, original);
-
-	kinc_log_args(level, final.string, args);
+	char buffer[4096];
+	tec_string_t pre = tec_string_create(prefix_string);
+	tec_string_t post = tec_string_create(format);
+	tec_string_t combo = tec_string_append(buffer, pre, post);
+	kinc_log_args(level, combo.string, args);
 }
 
 #ifndef TEC_LOG_ALWAYS_VERBOSE
 void
-tec_log_internal_info(char const * format, char const * prefix, ...)
+tec_log_internal_info(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_INFO, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_INFO, args);
 	va_end(args);
 }
 
 void 
-tec_log_internal_warn(char const * format, char const * prefix, ...)
+tec_log_internal_warn(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_WARNING, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_WARNING, args);
 	va_end(args);
 }
 
 void 
-tec_log_internal_error(char const * format, char const * prefix, ...)
+tec_log_internal_error(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_ERROR, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_ERROR, args);
 	va_end(args);
 }
 #else
-tec_log_internal_info(char const * format, char const * prefix, ...)
+tec_log_internal_info(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_INFO, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_INFO, args);
 	va_end(args);
 }
 
 void 
-tec_log_internal_warn(char const * format, char const * prefix, ...)
+tec_log_internal_warn(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_WARNING, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_WARNING, args);
 	va_end(args);
 }
 
 void 
-tec_log_internal_error(char const * format, char const * prefix, ...)
+tec_log_internal_error(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_ERROR, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_ERROR, args);
 	va_end(args);
 }
 #endif
 
 void
-tec_log_internal_info_verbose(char const * format, char const * prefix, ...)
+tec_log_internal_info_verbose(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_INFO, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_INFO, args);
 	va_end(args);
 }
 
 void 
-tec_log_internal_warn_verbose(char const * format, char const * prefix, ...)
+tec_log_internal_warn_verbose(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_WARNING, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_WARNING, args);
 	va_end(args);
 }
 
 void 
-tec_log_internal_error_verbose(char const * format, char const * prefix, ...)
+tec_log_internal_error_verbose(char const * prefix, char const * format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	tec_log_modify(format, prefix, KINC_LOG_LEVEL_ERROR, args);
+	tec_log_modify(prefix, format, KINC_LOG_LEVEL_ERROR, args);
 	va_end(args);
 }
--- Sources/tectonic/debug/log.h
+++ Sources/tectonic/debug/log.h
@@ -6,13 +6,13 @@
 
 #ifdef TEC_LOG_LOGGING_ENABLED
 
-#define tec_log_info(format, ...) tec_log_internal_info(format, "[TEC-INFO]::[" TEC_LOG_MODULE_NAME "] ", __VA_ARGS__)
-#define tec_log_warn(format, ...) tec_log_internal_warn(format, "[TEC-WARN]::[" TEC_LOG_MODULE_NAME "] " , __VA_ARGS__)
-#define tec_log_error(format, ...) tec_log_internal_error(format, "[TEC-ERROR]::[" TEC_LOG_MODULE_NAME "] ", __VA_ARGS__)
+#define tec_log_info(format, ...) tec_log_internal_info("[TEC-INFO]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
+#define tec_log_warn(format, ...) tec_log_internal_warn("[TEC-WARN]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
+#define tec_log_error(format, ...) tec_log_internal_error("[TEC-ERROR]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
 
-#define tec_log_info_verbose(format, ...) tec_log_internal_info_verbose(format, "[TEC-INFO]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", __VA_ARGS__)
-#define tec_log_warn_verbose(format, ...) tec_log_internal_warn_verbose(format, "[TEC-WARN]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", __VA_ARGS__)
-#define tec_log_error_verbose(format, ...) tec_log_internal_error_verbose(format, "[TEC-ERROR]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", __VA_ARGS__)
+#define tec_log_info_verbose(format, ...) tec_log_internal_info_verbose("[TEC-INFO]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", format, __VA_ARGS__)
+#define tec_log_warn_verbose(format, ...) tec_log_internal_warn_verbose("[TEC-WARN]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", format, __VA_ARGS__)
+#define tec_log_error_verbose(format, ...) tec_log_internal_error_verbose("[TEC-ERROR]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", format,  __VA_ARGS__)
 
 #else
 
