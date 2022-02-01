9e17759e0e7b599e4c3804d4d5dc00d5cb7df755
z <zshoals@gmail.com>
2022-02-01 15:17:04 -0600

    make type attachment legacy

--- Sources/tectonic/ds/template.h
+++ Sources/tectonic/ds/template.h
@@ -7,6 +7,8 @@
 #define _tec_paste(a, b) _tec_concat(a, b)
 
 #define _tec_bfa_makename(prefix, type, count) _tec_paste(_tec_paste(prefix, type), _tec_paste(_, count))
-#define _tec_bfa_makename_t(prefix, type, count) _tec_paste(_tec_bfa_makename(prefix, type, count), _t)
+
+//This is the same for now, but at one point appended _t, leaving here for legacy purposes
+#define _tec_bfa_makename_t(prefix, type, count) _tec_bfa_makename(prefix, type, count)
 
 #define _tec_func(func_suffix) _tec_paste(_tec_me, func_suffix)
