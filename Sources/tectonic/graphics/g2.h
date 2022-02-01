fdf34d14060565001bce2625e267a20cf1da78f8
z <zshoals@gmail.com>
2022-01-31 23:34:36 -0600

    g2 and material work

--- Sources/tectonic/graphics/g2.h
+++ Sources/tectonic/graphics/g2.h
@@ -2,12 +2,27 @@
 
 #include "../lib/HandmadeMath.h"
 #include "color.h"
+#include "renderer.h"
 
 //Contains commands for things like drawing rects and lines. Textured, untextured, etc.
 //Batches and submits everything to renderer
 
 //Rect -> Center positions, size, color (this one's untextured)
 //Well actually they're all textured, we're going to make a 1x1 white pixel to avoid the shader program change
+typedef struct
+tec_g2_context
+{
+	tec_renderer_context_t r_ctx; //STUFF HERE
+}
+tec_g2_context_t;
+
+//sets the context to use for drawing
+//Everything is automatically spritebatched
+void tec_g2_begin(tec_g2_context_t * context);
+//Releases context, probably ends drawing
+void tec_g2_end(void);
+
+
 void tec_g2_draw_rect(float cx, float cy, float size, tec_color_t color);
 void tec_g2_draw_line(hmm_v2 p1, hmm_v2 p2, float thickness, tec_color_t color);
 
--- Sources/tectonic/graphics/material.c
+++ Sources/tectonic/graphics/material.c
@@ -1,12 +1,15 @@
 #include "material.h"
 #include "kinc/graphics4/pipeline.h"
 #include <assert.h>
+#include "../frequent.h"
+
+tec_material_material_t default_textured;
 
 void 
-tec_material_initialize(tec_material_material_t * mat, char const * name, kinc_g4_pipeline_t pipeline)
+tec_material_initialize(tec_material_material_t * mat, char const * name, kinc_g4_pipeline_t * pipeline)
 {
 	mat->name = name;
-	mat->pipeline = pipeline;
+	mat->pipeline = DEREF(pipeline);
 
 	for (int i = 0; i < TEC_MATERIAL_MAX_TEXTURE_UNITS; i++)
 	{
@@ -34,45 +37,4 @@ tec_material_assign_uniform(tec_material_material_t * mat, char const * name, te
 	mat->uniforms[slot].data = data;
 	mat->uniforms[slot].type = type;
 	mat->uniforms[slot].location = kinc_g4_pipeline_get_constant_location(&mat->pipeline, name);
-}
-
-void
-tec_material_pipeline_blend_mode_helper(kinc_g4_pipeline_t * pipeline, tec_material_pipeline_blending_e blending_mode)
-{
-	switch (blending_mode)
-	{
-		case TEC_BLENDING_DISABLED:
-			pipeline->blend_source = KINC_G4_BLEND_ONE;
-			pipeline->blend_destination = KINC_G4_BLEND_ZERO;
-			break;
-
-		case TEC_BLENDING_NORMAL:
-			pipeline->blend_source = KINC_G4_BLEND_ONE;
-			pipeline->blend_destination = KINC_G4_BLEND_INV_SOURCE_ALPHA;
-			break;
-
-		case TEC_BLENDING_ADD:
-			pipeline->blend_source = KINC_G4_BLEND_ONE;
-			pipeline->blend_destination = KINC_G4_BLEND_ONE;
-			break;
-
-		case TEC_BLENDING_MULTIPLY:
-			pipeline->blend_source = KINC_G4_BLEND_DEST_COLOR;
-			pipeline->blend_destination = KINC_G4_BLEND_INV_SOURCE_ALPHA;
-			break;
-
-		case TEC_BLENDING_MASK_WITH_COLOR:
-			pipeline->blend_source = KINC_G4_BLEND_SOURCE_ALPHA;
-			pipeline->blend_destination = KINC_G4_BLEND_SOURCE_ALPHA;
-			break;
-
-		case TEC_BLENDING_MASK:
-			pipeline->blend_source = KINC_G4_BLEND_ZERO;
-			pipeline->blend_destination = KINC_G4_BLEND_SOURCE_ALPHA;
-			break;
-		
-		default:
-			assert(0 && "Should not be able to reach default in blend_mode_helper");
-	}
-
 }
