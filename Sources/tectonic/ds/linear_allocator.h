aa47a95e67a97caf3bcb45b9b37b7e8a51d08b1d
z <zshoals@gmail.com>
2022-02-01 19:09:36 -0600

    fixup linear allocator by defaulting to tec_bytes always

--- Sources/tectonic/ds/linear_allocator.h
+++ Sources/tectonic/ds/linear_allocator.h
@@ -1,15 +1,19 @@
 #include "template.h"
+#include "../util/memkit.h"
 #if defined(template_type) && defined(template_array_size)
+//Hacky...
+#define template_type tec_byte_t
 
-#define _tec_self linalloc_
+#define _tec_self linalloc_tec_bytes_t_
 
-#define _tec_me _tec_bfa_makename(_tec_self, template_type, template_array_size)
-#define _tec_me_t _tec_bfa_makename_t(_tec_self, template_type, template_array_size)
+#define _tec_me _tec_paste(_tec_self, template_array_size)
+#define _tec_me_t _tec_paste(_tec_self, template_array_size)
 
 //Note: When including this with these includes, this MIGHT mess things up when included in a .c file, as they do not use #pragma once
 //Be careful about that.
 #include <stddef.h>
 #include <assert.h>
+
 
 typedef struct
 _tec_me
@@ -30,7 +34,7 @@ _tec_func(_initialize)(_tec_me_t * allocator)
 template_type *
 _tec_func(_allocate)(_tec_me_t * allocator, size_t size)
 {
-	assert(((allocator->current_head + size) - &allocator->buffer) < (&allocator->buffer[max_length]) && "Allocation would overrun buffer");
+	assert(((allocator->current_head + size) - &allocator->buffer) < (&allocator->buffer[allocator->max_length]) && "Allocation would overrun buffer");
 
 	template_type * old_location = allocator->current_head;
 	allocator->current_head += size;
