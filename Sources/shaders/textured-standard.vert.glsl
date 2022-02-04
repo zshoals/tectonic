1f51bdea69887dcb67fb4464173aa3a6db7a7301
z <zshoals@gmail.com>
2022-02-04 14:41:21 -0600

    make the texture-vert-shader actually texture

--- Sources/shaders/textured-standard.vert.glsl
+++ Sources/shaders/textured-standard.vert.glsl
@@ -1,11 +1,14 @@
 #version 450
 
 in vec3 vertexPosition;
+in vec2 vertexUV;
 in vec4 vertexColor;
 uniform mat4 projectionMatrix;
-out vec4 fragmentColor;
+out vec2 texCoord;
+out vec4 color;
 
 void main() {
 	gl_Position = projectionMatrix * vec4(vertexPosition, 1.0);
-	fragmentColor = vertexColor;
+	texCoord = vertexUV;
+	color = vertexColor;
 }
