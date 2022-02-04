9aa12eb262012219b87eee3f89a140b827a983d2
z <zshoals@gmail.com>
2022-02-03 21:16:12 -0600

    make warn and error stand out more

--- Sources/tectonic/debug/log.h
+++ Sources/tectonic/debug/log.h
@@ -5,8 +5,8 @@
 #ifdef TEC_LOG_LOGGING_ENABLED
 
 #define tec_log_info(format, ...) tec_log_internal_info("[TEC-INFO]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
-#define tec_log_warn(format, ...) tec_log_internal_warn("[TEC-WARN]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
-#define tec_log_error(format, ...) tec_log_internal_error("[TEC-ERROR]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
+#define tec_log_warn(format, ...) tec_log_internal_warn("[TEC-!!WARN!!]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
+#define tec_log_error(format, ...) tec_log_internal_error("[TEC-!!!!!ERROR!!!!!]::[" TEC_LOG_MODULE_NAME "] ", format, __VA_ARGS__)
 
 #define tec_log_info_verbose(format, ...) tec_log_internal_info_verbose("[TEC-INFO]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", format, __VA_ARGS__)
 #define tec_log_warn_verbose(format, ...) tec_log_internal_warn_verbose("[TEC-WARN]::[" TEC_LOG_MODULE_NAME "]::[File-" __FILE__ "]::[Line-" TEC_EXPANDER(__LINE__) "] ", format, __VA_ARGS__)
