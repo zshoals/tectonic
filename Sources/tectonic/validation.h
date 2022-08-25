9662c55e952b557935acde3a693c84de6bce3532
z <zshoals@gmail.com>
2022-08-24 20:12:10 -0500

    actually make assertions work by wrapping expr and messages properly

--- Sources/tectonic/validation.h
+++ Sources/tectonic/validation.h
@@ -8,13 +8,16 @@
 	#define NDEBUG
 #endif
 
+//NOTE: asserts with messages need to be wrapped like this:
+// assert (( MSG, EXPR ) )
+//in order to work properly.
 #ifndef TEC_DISABLE_UNFORCED_ASSERTS
-	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) assert((Message), (Pointer))
-	#define DEBUG_ENSURE_TRUE(Expression, Message) assert((Message), (Expression))
-	#define DEBUG_ENSURE_FALSE(Expression, Message) assert((Message), !(Expression))
-	#define DEBUG_ENSURE_INT_EQUALS(Int1, Int2, Message) assert((Message), (Int1) == (Int2))
-	#define DEBUG_ENSURE_NUMBER_IN_RANGE(Value, Lower, Upper, Message) assert( (Message), ((Lower) <= (Value) && (Value) <= (Upper)) )
-	#define DEBUG_ENSURE_KILL_NOW(Message) assert( (Message), 0)
+	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) assert(( (Message), (Pointer) ))
+	#define DEBUG_ENSURE_TRUE(Expression, Message) assert(( (Message), (Expression) ))
+	#define DEBUG_ENSURE_FALSE(Expression, Message) assert(( (Message), !(Expression) ))
+	#define DEBUG_ENSURE_INT_EQUALS(Int1, Int2, Message) assert(( (Message), (Int1) == (Int2) ))
+	#define DEBUG_ENSURE_NUMBER_IN_RANGE(Value, Lower, Upper, Message) assert(( (Message), ((Lower) <= (Value) && (Value) <= (Upper)) ))
+	#define DEBUG_ENSURE_KILL_NOW(Message) assert(( (Message), 0 ))
 #else
 	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) ((void)0)
 	#define DEBUG_ENSURE_TRUE(Expression, Message) ((void)0)
@@ -26,10 +29,15 @@
 
 
 //These assertions are never disabled, even in release builds
-#define ENSURE_PTR_NOT_NULL(Pointer, Message) assert((Message), (Pointer))
-#define ENSURE_TRUE(Expression, Message) assert((Message), (Expression))
-#define ENSURE_FALSE(Expression, Message) assert((Message), !(Expression))
-#define ENSURE_INT_EQUALS(Int1, Int2, Message) assert((Message), (Int1) == (Int2))
-#define ENSURE_NUMBER_IN_RANGE(Value, Lower, Upper, Message) assert( (Message), ((Lower) <= (Value) && (Value) <= (Upper)) )
-#define ENSURE_KILL_NOW(Message) assert( (Message), 0)
-#define ENSURE_UNREACHABLE(Message) assert( (Message), 0)
+#define ENSURE_PTR_NOT_NULL(Pointer, Message) assert(( (Message), (Pointer) ))
+#define ENSURE_TRUE(Expression, Message) assert(( (Message), (Expression) ))
+#define ENSURE_FALSE(Expression, Message) assert(( (Message), !(Expression) ))
+#define ENSURE_INT_EQUALS(Int1, Int2, Message) assert(( (Message), (Int1) == (Int2) ))
+#define ENSURE_NUMBER_IN_RANGE(Value, Lower, Upper, Message) assert(( (Message), ((Lower) <= (Value) && (Value) <= (Upper)) ))
+//For when you absolutely need to kill the program right now
+#define ENSURE_KILL_NOW(Message) assert(( (Message), 0))
+//Intended for things like switch cases with unreachable and unwanted default outcomes, for instance
+#define ENSURE_UNREACHABLE(Message) assert(( (Message), 0 ))
+//Intended for unimplemented functions and features
+#define ENSURE_UNIMPLEMENTED() assert(( "Function is unimplemented.", 0 ))
+
