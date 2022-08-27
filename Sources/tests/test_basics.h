6633733fd1b3dca6001e12a7de957f85c52dc3f7
z <zshoals@gmail.com>
2022-08-26 19:14:56 -0500

    remove unnecessary declarations

--- Sources/tests/test_basics.h
+++ Sources/tests/test_basics.h
@@ -3,8 +3,3 @@
 
 //This is a control group, used to make sure that the basic TEST functions work appropriately
 void test_all_integer_basic_operations(void);
-
-bool test_integer_addition(void);
-bool test_integer_subtraction(void);
-bool test_integer_division(void);
-bool test_integer_multiplication(void);
