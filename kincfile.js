4d959278bf59917201fecb2eba75e2705e9692c1
z <zshoals@gmail.com>
2022-08-26 19:12:49 -0500

    make note of optional defines

--- kincfile.js
+++ kincfile.js
@@ -8,3 +8,7 @@ project.setDebugDir('Deployment');
 project.addDefine('TEC_LOG_LOGGING_ENABLED=1');
 
 resolve(project);
+
+//Possible flags are:
+//TEC_DISABLE_UNFORCED_ASSERTS to disable any non-mandatory assertions
+//TEC_DISABLE_TESTS to disable the runtime tests launched before main
