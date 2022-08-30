f1396e0a2bf7f3bac55bbba366bf44c41b18bfb1
z <zshoals@gmail.com>
2022-08-29 23:23:41 -0500

    add memory operations

---
+++ Sources/tectonic/memory/memory.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef unsigned char tec_bytes;
+
+#define TEC_KILOBYTES(AMOUNT) ((AMOUNT) * 1024)
+#define TEC_MEGABYTES(AMOUNT) (TEC_KILOBYTES(AMOUNT) * 1024)
+#define TEC_GIGABYTES(AMOUNT) (TEC_MEGABYTES(AMOUNT) * 1024)
+
+//Hopefully portable way to find alignment in C99
+#define alignof(type) offsetof(struct { char c; type d; }, d)
