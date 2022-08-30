3c380700fde9fe3792d4641ce5c7969ffca54dac
z <zshoals@gmail.com>
2022-08-29 23:29:57 -0500

    source the alignof operation

--- Sources/tectonic/memory/memory.h
+++ Sources/tectonic/memory/memory.h
@@ -8,5 +8,6 @@ typedef unsigned char tec_bytes;
 #define TEC_MEGABYTES(AMOUNT) (TEC_KILOBYTES(AMOUNT) * 1024)
 #define TEC_GIGABYTES(AMOUNT) (TEC_MEGABYTES(AMOUNT) * 1024)
 
+//Source: https://stackoverflow.com/a/28464194
 //Hopefully portable way to find alignment in C99
 #define alignof(type) offsetof(struct { char c; type d; }, d)
