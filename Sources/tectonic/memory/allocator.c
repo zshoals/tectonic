cf7beadf3a0cb80fa00f28fa8b01ebb0d5d7ed66
z <zshoals@gmail.com>
2022-09-01 04:09:01 -0500

    insert and adjust safety checks regarding alignment

--- Sources/tectonic/memory/allocator.c
+++ Sources/tectonic/memory/allocator.c
@@ -1,11 +1,12 @@
+#include "allocator.h"
 #include "tectonic/validation.h"
 #include "tectonic/tcommon.h"
-#include "allocator.h"
+#include "kinc/log.h"
 
 bool allocator_internal_is_power_of_two(size_t value)
 {
 	ENSURE_TRUE(value > 0, "Expected that the target alignment was greater than 0");
-	return value && ((value & (value - 1))) == 0;
+	return ((value & (value - 1))) == 0;
 }
 
 //Returns a memory buffer offset for a byte array
@@ -18,11 +19,18 @@ size_t allocator_internal_align_forward(size_t current_buffer_top, size_t align)
 	return current_buffer_top + align_forward;
 }
 
+//Note: It is assumed that the memory you provide is already 16 byte aligned. This should be the case if you received
+//your memory from the stack or from a malloc call.
 allocator_t allocator_create(tec_bytes * memory_buffer, size_t memory_buffer_size, allocator_mode_e alloc_mode)
 {
 	ENSURE_PTR_NOT_NULL(memory_buffer, "Tried to create an allocator with a null memory buffer");
 	ENSURE_TRUE(memory_buffer_size > 0, "Mem buffer size must be greater than 0");
-	ENSURE_TRUE((uintptr_t)memory_buffer % DEFAULT_ALIGNMENT == 0, "Tried to create an allocator with a non-default aligned memory region...should be 16 byte aligned.");
+
+	if (CAST(uintptr_t)memory_buffer % DEFAULT_ALIGNMENT != 0)
+	{
+		kinc_log(KINC_LOG_LEVEL_WARNING, "Warning: Allocator was created, but the initial memory buffer was not 16-byte aligned. You may wish to verify that this was intended.");
+		ENSURE_UNREACHABLE("Actually, for now, we don't support > 16 byte buffers, laziness.");
+	}
 
 	switch (alloc_mode)
 	{
@@ -51,6 +59,14 @@ void * allocator_aligned_malloc(allocator_t * a, size_t target_alignment, size_t
 	//Some sort of memory tracking...
 	ENSURE_PTR_NOT_NULL(a, "Provided allocator was null");
 	ENSURE_TRUE(allocator_internal_is_power_of_two(target_alignment), "Target alignment needs to be a valid power of 2");
+	ENSURE_TRUE(elem_count > 0, "Elements provided to be allocated was 0");
+	ENSURE_TRUE(elem_size > 0, "Element size was 0 or less while allocating");
+	//Why the following ensure is here:
+	//We currently assume the incoming target alignment is 16 byte aligned because that's what malloc/stack provides
+	//Since we're operating on offsets and NOT actual pointer values, we can't get the actual pointer address right now
+	//If you want to support values greater than 16 bytes, this will need to be changed to adjust the initial address
+	//to the correct alignment
+	ENSURE_TRUE(target_alignment <= DEFAULT_ALIGNMENT, "Target alignment exceeeded the maximum supported alignment");
 
 	switch (a->mode)
 	{
@@ -59,9 +75,6 @@ void * allocator_aligned_malloc(allocator_t * a, size_t target_alignment, size_t
 			if ((a->memory_top_offset & target_alignment) == 0)
 			{
 				//If the current target_alignment is already fulfilled by the memory offset, we're done
-				//NOTE: We assume that the original memory buffer in the allocator is aligned to fulfill any type
-				//This may or may not be true for some types (128 wide float groups?)
-				//Should be ok for anything 16 or less though
 				target_offset = a->memory_top_offset;
 			}
 			else
