24ca60e57ffa42b31031e42f4a567a159571ff0c
z <zshoals@gmail.com>
2022-09-15 07:14:59 -0500

    Completely rework validation to allow for value output and stronger value typing

--- Sources/tectonic/validation.h
+++ Sources/tectonic/validation.h
@@ -8,35 +8,125 @@
 	#define NDEBUG
 #endif
 
-//NOTE: asserts with messages need to be wrapped with double parenthesis like this:
-// assert (( MSG, EXPR ))
-//in order to work properly. It makes sense, assert only takes a single parameter
+void tec_validation_ensure_int_gt(long long a, long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_int_gte(long long a, long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_int_lt(long long a, long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_int_lte(long long a, long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_int_equals(long long a, long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_int_in_range(long long value, long long lower, long long upper, char const * message, char const * file, int line);
+
+void tec_validation_ensure_uint_gt(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_uint_gte(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_uint_lt(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_uint_lte(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_uint_equals(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
+void tec_validation_ensure_uint_in_range(unsigned long long value, unsigned long long lower, unsigned long long upper, char const * message, char const * file, int line);
+
+void tec_validation_ensure_float_gt(long double a, long double b, char const * message, char const * file, int line);
+void tec_validation_ensure_float_gte(long double a, long double b, char const * message, char const * file, int line);
+void tec_validation_ensure_float_lt(long double a, long double b, char const * message, char const * file, int line);
+void tec_validation_ensure_float_lte(long double a, long double b, char const * message, char const * file, int line);
+void tec_validation_ensure_float_equals(long double a, long double b, char const * message, char const * file, int line);
+void tec_validation_ensure_float_in_range(long double value, long double lower, long double upper, char const * message, char const * file, int line);
+
+void tec_validation_ensure_ptr_not_null(void * ptr, char const * message, char const * file, int line);
+void tec_validation_ensure_ptr_is_null(void * ptr, char const * message, char const * file, int line);
+
 #ifndef TEC_DISABLE_UNFORCED_ASSERTS
-	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) assert(( (Message), (Pointer) ))
 	#define DEBUG_ENSURE_TRUE(Expression, Message) assert(( (Message), (Expression) ))
 	#define DEBUG_ENSURE_FALSE(Expression, Message) assert(( (Message), !(Expression) ))
-	#define DEBUG_ENSURE_INT_EQUALS(Int1, Int2, Message) assert(( (Message), (Int1) == (Int2) ))
-	#define DEBUG_ENSURE_NUMBER_IN_RANGE(Value, Lower, Upper, Message) \
-		assert(( (Message), ((Lower) <= (Value) && (Value) <= (Upper)) ))
 	#define DEBUG_ENSURE_KILL_NOW(Message) assert(( (Message), 0 ))
+
+	#define DEBUG_ENSURE_INT_GT(A, B, Message) tec_validation_ensure_int_gt(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_INT_GTE(A, B, Message) tec_validation_ensure_int_gte(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_INT_LT(A, B, Message) tec_validation_ensure_int_lt(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_INT_LTE(A, B, Message) tec_validation_ensure_int_lte(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_INT_EQUALS(A, B, Message) tec_validation_ensure_int_equals(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_INT_IN_RANGE(Value, Lower, Upper, Message) tec_validation_ensure_int_in_range(Value, Lower, Upper, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_INT_GTZERO(Value, Message) tec_validation_ensure_int_gt(Value, 0, Message, __FILE__, __LINE__)
+
+	#define DEBUG_ENSURE_UINT_GT(A, B, Message) tec_validation_ensure_uint_gt(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_UINT_GTE(A, B, Message) tec_validation_ensure_uint_gte(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_UINT_LT(A, B, Message) tec_validation_ensure_uint_lt(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_UINT_LTE(A, B, Message) tec_validation_ensure_uint_lte(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_UINT_EQUALS(A, B, Message) tec_validation_ensure_uint_equals(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_UINT_IN_RANGE(Value, Lower, Upper, Message) tec_validation_ensure_uint_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
+	#define DEBUG_ENSURE_UINT_GTZERO(Value, Message) tec_validation_ensure_uint_gt(Value, 0, Message, __FILE__, __LINE__)
+
+	#define DEBUG_ENSURE_FLOAT_GT(A, B, Message) tec_validation_ensure_float_gt(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_FLOAT_GTE(A, B, Message) tec_validation_ensure_float_gte(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_FLOAT_LT(A, B, Message) tec_validation_ensure_float_lt(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_FLOAT_LTE(A, B, Message) tec_validation_ensure_float_lte(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_FLOAT_EQUALS(A, B, Message) tec_validation_ensure_float_equals(A, B, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_FLOAT_IN_RANGE(Value, Lower, Upper, Message) tec_validation_ensure_float_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
+
+	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) tec_validation_ensure_ptr_not_null(Pointer, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_PTR_IS_NULL(Pointer, Message) tec_validation_ensure_ptr_is_null(Pointer, Message, __FILE__, __LINE__)
+
 #else
-	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) ((void)0)
 	#define DEBUG_ENSURE_TRUE(Expression, Message) ((void)0)
 	#define DEBUG_ENSURE_FALSE(Expression, Message) ((void)0)
-	#define DEBUG_ENSURE_INT_EQUALS(Int1, Int2, Message) ((void)0)
-	#define DEBUG_ENSURE_NUMBER_IN_RANGE(Value, Lower, Upper, Message) ((void)0)
 	#define DEBUG_ENSURE_KILL_NOW(Message) ((void)0)
+
+	#define DEBUG_ENSURE_INT_GT(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_INT_GTE(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_INT_LT(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_INT_LTE(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_INT_EQUALS(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_INT_IN_RANGE(Value, Lower, Upper, Message) ((void)0)
+	#define DEBUG_ENSURE_INT_GTZERO(Value, Message) ((void)0)
+
+	#define DEBUG_ENSURE_UINT_GT(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_UINT_GTE(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_UINT_LT(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_UINT_LTE(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_UINT_EQUALS(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_UINT_IN_RANGE(Value, Lower, Upper, Message) ((void)0)
+	#define DEBUG_ENSURE_UINT_GTZERO(Value, Message) ((void)0)
+
+	#define DEBUG_ENSURE_FLOAT_GT(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_FLOAT_GTE(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_FLOAT_LT(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_FLOAT_LTE(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_FLOAT_EQUALS(A, B, Message) ((void)0)
+	#define DEBUG_ENSURE_FLOAT_IN_RANGE(Value, Lower, Upper, Message) ((void)0)
+
+	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) ((void)0)
+	#define DEBUG_ENSURE_PTR_IS_NULL(Pointer, Message) ((void)0)
 #endif
 
 
 //These assertions are never disabled, even in release builds
-#define ENSURE_PTR_NOT_NULL(Pointer, Message) assert(( (Message), (Pointer) ))
 #define ENSURE_TRUE(Expression, Message) assert(( (Message), (Expression) ))
 #define ENSURE_FALSE(Expression, Message) assert(( (Message), !(Expression) ))
-#define ENSURE_INT_EQUALS(Int1, Int2, Message) assert(( (Message), (Int1) == (Int2) ))
-#define ENSURE_NUMBER_IN_RANGE(Value, Lower, Upper, Message) \
-	assert(( (Message), ((Lower) <= (Value) && (Value) <= (Upper)) ))
-//For when you absolutely need to kill the program right now
+
+#define ENSURE_INT_GT(A, B, Message) tec_validation_ensure_int_gt(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_INT_GTE(A, B, Message) tec_validation_ensure_int_gte(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_INT_LT(A, B, Message) tec_validation_ensure_int_lt(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_INT_LTE(A, B, Message) tec_validation_ensure_int_lte(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_INT_EQUALS(A, B, Message) tec_validation_ensure_int_equals(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_INT_IN_RANGE(Value, Lower, Upper, Message) tec_validation_ensure_int_in_range(Value, Lower, Upper, Message, __FILE__, __LINE__)
+#define ENSURE_INT_GTZERO(Value, Message) tec_validation_ensure_int_gt(Value, 0, Message, __FILE__, __LINE__)
+
+#define ENSURE_UINT_GT(A, B, Message) tec_validation_ensure_uint_gt(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_UINT_GTE(A, B, Message) tec_validation_ensure_uint_gte(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_UINT_LT(A, B, Message) tec_validation_ensure_uint_lt(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_UINT_LTE(A, B, Message) tec_validation_ensure_uint_lte(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_UINT_EQUALS(A, B, Message) tec_validation_ensure_uint_equals(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_UINT_IN_RANGE(Value, Lower, Upper, Message) tec_validation_ensure_uint_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
+#define ENSURE_UINT_GTZERO(Value, Message) tec_validation_ensure_uint_gt(Value, 0, Message, __FILE__, __LINE__)
+
+#define ENSURE_FLOAT_GT(A, B, Message) tec_validation_ensure_float_gt(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_FLOAT_GTE(A, B, Message) tec_validation_ensure_float_gte(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_FLOAT_LT(A, B, Message) tec_validation_ensure_float_lt(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_FLOAT_LTE(A, B, Message) tec_validation_ensure_float_lte(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_FLOAT_EQUALS(A, B, Message) tec_validation_ensure_float_equals(A, B, Message, __FILE__, __LINE__)
+#define ENSURE_FLOAT_IN_RANGE(Value, Lower, Upper, Message) tec_validation_ensure_float_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
+
+#define ENSURE_PTR_NOT_NULL(Pointer, Message) tec_validation_ensure_ptr_not_null(Pointer, Message, __FILE__, __LINE__)
+#define ENSURE_PTR_IS_NULL(Pointer, Message) tec_validation_ensure_ptr_is_null(Pointer, Message, __FILE__, __LINE__)
+
+//Something must die if you reach this point
 #define ENSURE_KILL_NOW(Message) assert(( (Message), 0 ))
 //Intended for things like switch cases with unreachable and unwanted default outcomes, for instance
 #define ENSURE_UNREACHABLE(Message) assert(( (Message), 0 ))
