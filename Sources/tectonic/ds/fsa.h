bc8a57732a8f34abda69112e6ed15304cc3e1d7b
z <zshoals@gmail.com>
2022-10-25 08:49:58 -0500

    new inline shorthand; rework iterator naming

--- Sources/tectonic/ds/fsa.h
+++ Sources/tectonic/ds/fsa.h
@@ -68,8 +68,35 @@ ds_fsa_self;
 #define fsa_capacity(DECLARED_TYPE) FSA_FUNC(DECLARED_TYPE, _capacity)()
 #define fsa_size_in_bytes(DECLARED_TYPE, FSA_PTR) FSA_FUNC(DECLARED_TYPE, _size_in_bytes)(FSA_PTR)
 
-//Unneeded as we no longer need to track the capacity of the fsa
-// static inline void FSA_FUNC(ds_fsa_self, _init)(ds_fsa_self * arr)
+//======================================
+//Iteration convenience
+//======================================
+
+#define __fsa_iterator_internal TEC_CONCAT(_tec_iterator_i_, __LINE__)
+#define __fsa_end_internal TEC_CONCAT(_tec_iterator_end_, __LINE__)
+
+//Iterates the entirety of the array's capacity, without accounting for the stack height
+#define foreach_fsa(CAPTURE, DECLARATION_TYPE, DATA_TYPE, ARRAY_PTR)\
+	size_t __fsa_iterator_internal = 0;\
+	const size_t __fsa_end_internal = FSA_FUNC(DECLARATION_TYPE, _capacity)();\
+	for (DATA_TYPE * CAPTURE = NULL; CAPTURE = &((ARRAY_PTR)->data[__fsa_iterator_internal]), __fsa_iterator_internal < __fsa_end_internal; ++__fsa_iterator_internal)
+
+#define __fsa_iterator_internal_limit TEC_CONCAT(_tec_iterator_i_, __LINE__)
+#define __fsa_end_internal_limit TEC_CONCAT(_tec_iterator_end_, __LINE__)
+
+//Iterates the fsa up to the max height of the stack
+#define foreach_fsa_stackmode(CAPTURE, DECLARATION_TYPE, DATA_TYPE, ARRAY_PTR)\
+	size_t __fsa_iterator_internal_limit = 0;\
+	const size_t __fsa_end_internal_limit = FSA_FUNC(DECLARATION_TYPE, _count)((ARRAY_PTR));\
+	for (DATA_TYPE * CAPTURE = NULL; CAPTURE = &((ARRAY_PTR)->data[__fsa_iterator_internal_limit]), __fsa_iterator_internal_limit < __fsa_end_internal_limit; ++__fsa_iterator_internal_limit)
+
+//======================================
+//
+//======================================
+
+//Initialization Unneeded as we no longer need to track the capacity of the fsa
+//Instead, just zero init with = {0}
+// DS_INLINE void FSA_FUNC(ds_fsa_self, _init)(ds_fsa_self * arr)
 // {
 // 	DEBUG_ENSURE_PTR_NOT_NULL(arr, "Pointer was null!");
 // 	arr->current_push_idx = 0;
@@ -77,22 +104,22 @@ ds_fsa_self;
 // 	memset(&arr->data[0], 0, (ds_element_count * sizeof(ds_type)) );
 // }
 
-static inline size_t FSA_FUNC(ds_fsa_self, _count)(ds_fsa_self * arr)
+DS_INLINE size_t FSA_FUNC(ds_fsa_self, _count)(ds_fsa_self * arr)
 {
 	return arr->current_push_idx;
 }
 
-static inline size_t FSA_FUNC(ds_fsa_self, _capacity)(void)
+DS_INLINE size_t FSA_FUNC(ds_fsa_self, _capacity)(void)
 {
 	return ds_element_count;
 }
 
-static inline size_t FSA_FUNC(ds_fsa_self, _size_in_bytes)(ds_fsa_self * arr)
+DS_INLINE size_t FSA_FUNC(ds_fsa_self, _size_in_bytes)(ds_fsa_self * arr)
 {
 	return sizeof(arr->data[0]) * FSA_FUNC(ds_fsa_self, _capacity)();
 }
 
-static inline void FSA_FUNC(ds_fsa_self, _push)(ds_fsa_self * arr, ds_type value)
+DS_INLINE void FSA_FUNC(ds_fsa_self, _push)(ds_fsa_self * arr, ds_type value)
 {
 	size_t max_cap = FSA_FUNC(ds_fsa_self, _capacity)();
 	ENSURE_UINT_LTE(arr->current_push_idx + 1, max_cap, "Array tried to push a value exceeding its capacity.");
@@ -100,7 +127,7 @@ static inline void FSA_FUNC(ds_fsa_self, _push)(ds_fsa_self * arr, ds_type value
 	arr->current_push_idx++;
 }
 
-static inline void FSA_FUNC(ds_fsa_self, _push_unsafe)(ds_fsa_self * arr, ds_type value)
+DS_INLINE void FSA_FUNC(ds_fsa_self, _push_unsafe)(ds_fsa_self * arr, ds_type value)
 {
 	size_t max_cap = FSA_FUNC(ds_fsa_self, _capacity)();
 	DEBUG_ENSURE_UINT_LTE(arr->current_push_idx + 1, max_cap, "Array tried to push a value exceeding its capacity.");
@@ -108,7 +135,7 @@ static inline void FSA_FUNC(ds_fsa_self, _push_unsafe)(ds_fsa_self * arr, ds_typ
 	arr->current_push_idx++;
 }
 
-static inline ds_type FSA_FUNC(ds_fsa_self, _pop)(ds_fsa_self * arr)
+DS_INLINE ds_type FSA_FUNC(ds_fsa_self, _pop)(ds_fsa_self * arr)
 {
 	size_t max_cap = FSA_FUNC(ds_fsa_self, _capacity());
 	ENSURE_UINT_LTE(arr->current_push_idx, max_cap, "Array push index exceeded array capacity while attempting to pop a value. Memory error?");
@@ -119,7 +146,7 @@ static inline ds_type FSA_FUNC(ds_fsa_self, _pop)(ds_fsa_self * arr)
 	return arr->data[target_idx];
 }
 
-static inline ds_type FSA_FUNC(ds_fsa_self, _pop_unsafe)(ds_fsa_self * arr)
+DS_INLINE ds_type FSA_FUNC(ds_fsa_self, _pop_unsafe)(ds_fsa_self * arr)
 {
 	size_t max_cap = FSA_FUNC(ds_fsa_self, _capacity());
 	DEBUG_ENSURE_UINT_LTE(arr->current_push_idx, max_cap, "Array push index exceeded array capacity while attempting to pop a value. Memory error?");
@@ -130,47 +157,44 @@ static inline ds_type FSA_FUNC(ds_fsa_self, _pop_unsafe)(ds_fsa_self * arr)
 	return arr->data[target_idx];
 }
 
-static inline ds_type const * FSA_FUNC(ds_fsa_self, _get)(ds_fsa_self * arr, size_t idx)
+DS_INLINE ds_type const * FSA_FUNC(ds_fsa_self, _get)(ds_fsa_self * arr, size_t idx)
 {
 	ds_internal_array_bounds_check( idx, FSA_FUNC(ds_fsa_self, _capacity)() );
 	return &arr->data[idx];
 }
 
-static inline ds_type const * FSA_FUNC(ds_fsa_self, _get_unsafe)(ds_fsa_self * arr, size_t idx)
+DS_INLINE ds_type const * FSA_FUNC(ds_fsa_self, _get_unsafe)(ds_fsa_self * arr, size_t idx)
 {
 	ds_internal_array_bounds_check_debug_only( idx, FSA_FUNC(ds_fsa_self, _capacity)() );
 	return &arr->data[idx];
 }
 
-static inline ds_type * FSA_FUNC(ds_fsa_self, _get_mut)(ds_fsa_self * arr, size_t idx)
+DS_INLINE ds_type * FSA_FUNC(ds_fsa_self, _get_mut)(ds_fsa_self * arr, size_t idx)
 {
 	ds_internal_array_bounds_check( idx, FSA_FUNC(ds_fsa_self, _capacity)() );
 	return &arr->data[idx];
 }
 
-static inline ds_type * FSA_FUNC(ds_fsa_self, _get_mut_unsafe)(ds_fsa_self * arr, size_t idx)
+DS_INLINE ds_type * FSA_FUNC(ds_fsa_self, _get_mut_unsafe)(ds_fsa_self * arr, size_t idx)
 {
 	ds_internal_array_bounds_check_debug_only( idx, FSA_FUNC(ds_fsa_self, _capacity)() );
 	return &arr->data[idx];
 }
 
-static inline void FSA_FUNC(ds_fsa_self, _set)(ds_fsa_self * arr, size_t idx, ds_type value)
+DS_INLINE void FSA_FUNC(ds_fsa_self, _set)(ds_fsa_self * arr, size_t idx, ds_type value)
 {
 	ds_internal_array_bounds_check( idx, FSA_FUNC(ds_fsa_self, _capacity)() );
 	arr->data[idx] = value;
 }
 
-static inline void FSA_FUNC(ds_fsa_self, _set_unsafe)(ds_fsa_self * arr, size_t idx, ds_type value)
+DS_INLINE void FSA_FUNC(ds_fsa_self, _set_unsafe)(ds_fsa_self * arr, size_t idx, ds_type value)
 {
 	ds_internal_array_bounds_check_debug_only( idx, FSA_FUNC(ds_fsa_self, _capacity)() );
 	arr->data[idx] = value;
 }
-
-//TODO(zshoals): Padding in structs disturbs equality checks, the user needs to provide a compare function
-//Fix this
 
 #ifdef ds_compare_function
-static inline ds_fsa_search_result FSA_FUNC(ds_fsa_self, _find)(ds_fsa_self * arr, ds_type * value)
+DS_INLINE ds_fsa_search_result FSA_FUNC(ds_fsa_self, _find)(ds_fsa_self * arr, ds_type * value)
 {
 	size_t const cap = FSA_FUNC(ds_fsa_self, _capacity)();
 	for (size_t i = 0; i < cap; ++i)
@@ -195,14 +219,9 @@ static inline ds_fsa_search_result FSA_FUNC(ds_fsa_self, _find)(ds_fsa_self * ar
 	};
 }
 
-static inline bool FSA_FUNC(ds_fsa_self, _has)(ds_fsa_self * arr, ds_type * value)
+DS_INLINE bool FSA_FUNC(ds_fsa_self, _has)(ds_fsa_self * arr, ds_type * value)
 {
 	return FSA_FUNC(ds_fsa_self, _find)(arr, value).value_found;
 }
 #endif
-
-#undef ds_type
-#undef ds_element_count
-#undef ds_fsa_self
-#undef ds_compare_function
 
