023252de25da42f595a1352e8e500b29b6ad79d9
z <zshoals@gmail.com>
2022-10-25 08:51:10 -0500

    add pretty comment

--- kincfile.js
+++ kincfile.js
@@ -189,13 +189,30 @@ generate_resource_files(
 // 	"music_resources"
 // )
 
+
+
+
+
+/*
+***************************************************
+***************************************************
+*********                                  ********
+*********   Actual Project Instantiation   ********
+*********                                  ********
+***************************************************
+***************************************************
+*/
+
+
+
+
 let project = new Project('Tectonic');
 project.addFile('Sources/**');
 project.addFiles('Sources/shaders/**');
 project.addFiles('Sources/assets/**');
 project.addIncludeDir('Sources');
-project.setDebugDir('Deployment');
 project.addDefine('TEC_LOG_LOGGING_ENABLED=1');
+project.setDebugDir('Deployment');
 
 resolve(project);
 
