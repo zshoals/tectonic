100913d4ef009a9cae5cd3f93e9d37d596c1dd6f
z <zshoals@gmail.com>
2022-10-28 13:44:09 -0500

    add initial filtered logging system

---
+++ Sources/tectonic/logging.c
@@ -0,0 +1,128 @@
+#include "logging.h"
+
+#include <stdio.h>
+#include "kinc/log.h"
+#include "inttypes.h"
+#include "tectonic/validation.h"
+
+//Allow all logs by default
+static tec_logging_flags active_filter = ~0;
+//Exclude nothing by default
+static tec_logging_flags active_exclusions = 0;
+
+void tec_internal_log_verbose(kinc_log_level_t level, char const * format, char const * file, size_t line, va_list args)
+{
+	char buffer[TEC_LOG_MAX_BUFFER_LEN];
+	switch (level)
+	{
+		case KINC_LOG_LEVEL_INFO:
+		{
+			snprintf(buffer, TEC_LOG_MAX_BUFFER_LEN, "[Info]::[%s]::[File: %s]::[Line: %zu]", format, file, line);
+			break;
+		}
+		case KINC_LOG_LEVEL_WARNING:
+		{
+			snprintf(buffer, TEC_LOG_MAX_BUFFER_LEN, "[WARNING]::[%s]::[File: %s]::[Line: %zu]", format, file, line);
+			break;
+		}
+		case KINC_LOG_LEVEL_ERROR:
+		{
+			snprintf(buffer, TEC_LOG_MAX_BUFFER_LEN, "[ERROR]::[%s]::[File: %s]::[Line: %zu]", format, file, line);
+			break;
+		}
+		default:
+			ENSURE_UNREACHABLE("Incorrect logging level set. Did you forget to select an appropriate Info, Warning, or Error filter for this log message?");
+	}
+
+	char const * string = &buffer[0];
+	kinc_log_args(level, string, args);
+}
+
+void tec_internal_log(kinc_log_level_t level, char const * format, char const * file, size_t line, va_list args)
+{
+	char buffer[TEC_LOG_MAX_BUFFER_LEN];
+	switch (level)
+	{
+		case KINC_LOG_LEVEL_INFO:
+		{
+			snprintf(buffer, TEC_LOG_MAX_BUFFER_LEN, "[Info]::[%s]", format);
+			break;
+		}
+		case KINC_LOG_LEVEL_WARNING:
+		{
+			snprintf(buffer, TEC_LOG_MAX_BUFFER_LEN, "[WARNING]::[%s]", format);
+			break;
+		}
+		case KINC_LOG_LEVEL_ERROR:
+		{
+			snprintf(buffer, TEC_LOG_MAX_BUFFER_LEN, "[ERROR]::[%s]", format);
+			break;
+		}
+		default:
+			ENSURE_UNREACHABLE("Incorrect logging level set. Did you forget to select an appropriate Info, Warning, or Error filter for this log message?");
+	}
+
+	char const * string = &buffer[0];
+	kinc_log_args(level, string, args);
+
+}
+
+void tec_log_set_filtering(tec_logging_flags flags, tec_logging_flags exclusions)
+{
+	active_filter = flags; 
+	active_exclusions = exclusions;
+}
+
+void tec_log_filtered(tec_logging_flags flags, char const * format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	tec_logging_flags filtered_flags;
+	//If flags contains any flag that's currently excluded, this log message
+	//will not be output
+	if (flags & active_exclusions)
+	{
+		filtered_flags = 0;
+	} 
+	else
+	{
+		filtered_flags = flags;
+	}
+
+	if (filtered_flags & active_filter)
+	{
+		if (flags & LOG_VERBOSE)
+		{
+			if (flags & LOG_INFO)
+			{
+				log_info_verbose(format, args);
+			}
+			if (flags & LOG_WARNING)
+			{
+				log_warn_verbose(format, args);
+			}
+			if (flags & LOG_ERROR)
+			{
+				log_error_verbose(format, args);
+			}
+		}
+		else
+		{
+			if (flags & LOG_INFO)
+			{
+				log_info(format, args);
+			}
+			if (flags & LOG_WARNING)
+			{
+				log_warn(format, args);
+			}
+			if (flags & LOG_ERROR)
+			{
+				log_error(format, args);
+			}
+		}
+	}
+
+	va_end(args);
+}
--- Sources/tectonic/logging.h
+++ Sources/tectonic/logging.h
@@ -1,3 +1,42 @@
 
 #pragma once
 
+#include <stdarg.h>
+#include "kinc/log.h"
+#include "tcommon.h"
+
+#define TEC_LOG_MAX_BUFFER_LEN 1024
+
+typedef enum
+{
+	LOG_INFO = (1 << 0),
+	LOG_WARNING = (1 << 1),
+	LOG_ERROR = (1 << 2),
+	LOG_VERBOSE = (1 << 3),
+
+	LOG_ENGINE = (1 << 4),
+	LOG_GAME = (1 << 5),
+	LOG_ECS = (1 << 6),
+	LOG_PHYSICS = (1 << 7),
+	LOG_GRAPHICS = (1 << 8),
+}      
+tec_logging_filter_e;
+
+typedef u64 tec_logging_flags;
+
+//Pass a list of flags to allow. Log message with even a single flag present will print,
+//exclusions will completely suppress a message with a selected tag
+void tec_log_set_filtering(tec_logging_flags flags, tec_logging_flags exclusions);
+
+void tec_internal_log_verbose(kinc_log_level_t level, char const * format, char const * file, size_t line, va_list args);
+void tec_internal_log(kinc_log_level_t level, char const * format, char const * file, size_t line, va_list args);
+void tec_log_filtered(tec_logging_flags flags, char const * format, ...);
+
+#define TEC_LOG(FILTER, FORMAT, ...) tec_log_filtered(FILTER, FORMAT, __VA_ARGS__)
+#define log_warn_verbose(FORMAT, ARGS) tec_internal_log_verbose(KINC_LOG_LEVEL_WARNING, FORMAT, __FILE__, __LINE__, ARGS)
+#define log_info_verbose(FORMAT, ARGS) tec_internal_log_verbose(KINC_LOG_LEVEL_INFO, FORMAT, __FILE__, __LINE__, ARGS)
+#define log_error_verbose(FORMAT, ARGS) tec_internal_log_verbose(KINC_LOG_LEVEL_ERROR, FORMAT, __FILE__, __LINE__, ARGS)
+
+#define log_warn(FORMAT, ARGS) tec_internal_log(KINC_LOG_LEVEL_WARNING, FORMAT, __FILE__, __LINE__, ARGS)
+#define log_info(FORMAT, ARGS) tec_internal_log(KINC_LOG_LEVEL_INFO, FORMAT, __FILE__, __LINE__, ARGS)
+#define log_error(FORMAT, ARGS) tec_internal_log(KINC_LOG_LEVEL_ERROR, FORMAT, __FILE__, __LINE__, ARGS)
