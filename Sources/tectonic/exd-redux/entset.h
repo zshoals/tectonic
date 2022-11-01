647a3fd210c16214ac667d5a835d59443520b4e8
z <zshoals@gmail.com>
2022-11-01 11:16:19 -0500

    ditto

--- Sources/tectonic/exd-redux/entset.h
+++ Sources/tectonic/exd-redux/entset.h
@@ -22,12 +22,12 @@ void exd_entset_copy_data_from(exd_entset_t * destination, exd_entset_t * source
 void exd_entset_set_slot(exd_entset_t * ents, exd_entity_t slot);
 void exd_entset_clear_slot(exd_entset_t * ents, exd_entity_t slot);
 
-bool exd_entset_block_is_empty(exd_entset_t * ents, exd_entity_t slot);
-bool exd_entset_block_is_full(exd_entset_t * ents, exd_entity_t slot);
+bool exd_entset_block_is_empty(exd_entset_t const * ents, exd_entity_t slot);
+bool exd_entset_block_is_full(exd_entset_t const * ents, exd_entity_t slot);
 
 void exd_entset_and(exd_entset_t * destination, exd_entset_t * source);
 void exd_entset_or(exd_entset_t * destination, exd_entset_t * source);
 void exd_entset_not(exd_entset_t * destination, exd_entset_t * source);
 
-bool exd_entset_slot_is_set(exd_entset_t * ents, exd_entity_t slot);
-bool exd_entset_slot_is_not_set(exd_entset_t * ents, exd_entity_t slot);
+bool exd_entset_slot_is_set(exd_entset_t const * ents, exd_entity_t slot);
+bool exd_entset_slot_is_not_set(exd_entset_t const * ents, exd_entity_t slot);
